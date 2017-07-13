d86d7b3c4fd10ba5336b4b551879f7a2ac2b35de
Hristo Vladev <hr.vladev@gmail.com>
2017-07-13 17:22:45 +0300

    Make the map interactive after a click

--- _pages/contacts.md
+++ _pages/contacts.md
@@ -22,4 +22,6 @@ title: Контакти
 [ул. Крум Попов 56-58, betahaus, ет. 2, София](https://goo.gl/maps/h7qvtpUxbvK2)
 {: .centered-text}
 
-<iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2933.106709804268!2d23.323079915466483!3d42.68028187916667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa8507c054abb1%3A0xbfd510ad7a016edf!2sbetahaus+%7C+sofia+-+coworking+space!5e0!3m2!1sbg!2sbg!4v1499871786515" frameborder="0" allowfullscreen></iframe>
+<div class="map-container">
+  <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2933.106709804268!2d23.323079915466483!3d42.68028187916667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa8507c054abb1%3A0xbfd510ad7a016edf!2sbetahaus+%7C+sofia+-+coworking+space!5e0!3m2!1sbg!2sbg!4v1499871786515" frameborder="0" allowfullscreen></iframe>
+</div>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+  initMenuSwitchForMobileVersion();
+  disableGoogleMapsSrollToZoom();
+}
+
+function initMenuSwitchForMobileVersion() {
   menu = document.getElementById("menu");
   menu_switch = document.getElementById("menu-switch");
 
@@ -7,6 +12,20 @@ window.onload = function() {
     menu_switch.classList.toggle("x");
   }
 
-  menu_switch.onclick = toggleMenu;
-  menu_switch.onkeydown = toggleMenu;
+  menu_switch.addEventListener("click", toggleMenu);
+  menu_switch.addEventListener("keydown", toggleMenu);
+}
+
+function disableGoogleMapsSrollToZoom() {
+  map = document.getElementById("map");
+  if ( map === null ) { return; }
+  map_container = map.parentNode;
+
+  map_container.addEventListener("click", function() {
+    map.classList.add("clicked");
+  })
+
+  map_container.addEventListener("mouseleave", function() {
+    map.classList.remove("clicked")
+  });
 }
